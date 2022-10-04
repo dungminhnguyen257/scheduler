4d5e814e8c9651b20caf3a5eeafd82be6ddf99b3
dungminhnguyen257 <nguy7670@mylaurier.ca>
2022-10-04 18:48:46 -0400

    completed DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--ful": props.spot === 0,
+  });
+
+  function formatSpot() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    return props.spots + " spots remaining";
+  }
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayItemClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpot(props.spots)} </h3>
+    </li>
+  );
+}
