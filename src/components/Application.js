1d4551fc5b0b3370c7c9679ab4a18c61be216923
dungminhnguyen257 <nguy7670@mylaurier.ca>
2022-10-08 22:59:50 -0400

    Built InterviewerListItem component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 import React from "react";
-import React, { useState } from "react";
+import { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import classNames from "classnames";
+import React from "react";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li onClick={props.setInterviewer} className={interviewerItemClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
