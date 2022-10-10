b1136eb1950723e86cb66f745f25272c81364df4
dungminhnguyen257 <nguy7670@mylaurier.ca>
2022-10-09 23:57:27 -0400

    Build appointment form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Show from "./Show";
+import Header from "./Header";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      {props.time && <Header time={props.time} />}
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
